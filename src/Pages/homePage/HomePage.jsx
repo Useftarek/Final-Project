8576205ba03eebd9b5888ffbb45bd3ece5748089
Useftarek <useftarek2@gmail.com>
2024-11-04 22:34:59 +0200

    update

--- src/Pages/homePage/HomePage.jsx
+++ src/Pages/homePage/HomePage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import BannerSection from '../../components/bannerSection/BannerSection'
+import ArrivalSection from '../../components/arrivalSection/ArrivalSection'
+import TopSellingSection from '../../components/topSellingSection/TopSellingSection'
 
 export default function HomePage() {
   return (
     <div>
       <BannerSection />
+      <ArrivalSection />
+      <hr />
+      <TopSellingSection />
     </div>
   )
 }
