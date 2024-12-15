2f7961f5f56d60305aeac17d8f9e2c1608f84424
Useftarek <useftarek2@gmail.com>
2024-12-14 19:02:30 -1000

    done

--- src/components/product_details_Addcart/Products_D_AddCart.jsx
+++ src/components/product_details_Addcart/Products_D_AddCart.jsx
@@ -1,27 +1,43 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from "react";
+import { CartContext } from "../../context/CartContext";
+import toast from "react-hot-toast";
 
-export default function Products_D_AddCart() {
-
-  const [count, setCount] = useState(0);
+export default function Products_D_AddCart({ product }) {
+  const [count, setCount] = useState(1);
+  const { addToCart } = useContext(CartContext);
 
   const increase = () => {
     setCount(count + 1);
-};
+  };
 
-const decrease = () => {
-  if (count > 0) {
+  const decrease = () => {
+    if (count > 1) {
       setCount(count - 1);
-  }
-};
+    }
+  };
 
-
+  const handleAddToCart = () => {
+    const productToAdd = { ...product, quantity: count };
+    addToCart(productToAdd);
+    toast.success("Product Add Successfully ");
+  };
 
   return (
-    <div className='product_add_container'>
+    <div className="product_add_container">
       <div className="Add_minues">
-      <p><span className="minus" onClick={decrease}>-</span> {count} <span className="plus" onClick={increase}>+</span></p>
+        <p>
+          <span className="minus" onClick={decrease}>
+            -
+          </span>{" "}
+          {count}{" "}
+          <span className="plus" onClick={increase}>
+            +
+          </span>
+        </p>
       </div>
-      <button className='add_to_cart-btn'> Add To Cart</button>
+      <button className="add_to_cart-btn" onClick={handleAddToCart}>
+        Add To Cart
+      </button>
     </div>
-  )
+  );
 }
